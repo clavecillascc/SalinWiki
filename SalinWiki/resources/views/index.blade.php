d864fb8e7048bc25008e6e60f56ff058b54fcfbc
Christian C. Clavecillas <clavecillascc@gmail.com>
2023-12-16 22:59:03 +0800

    initial

---
+++ SalinWiki/resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SalinWiki</title>
+</head>
+<body>
+    <p>
+        hello world
+    </p>
+
+    <h1>
+        <a href="{{ route('login') }}">Login</a> 
+    </h1>
+
+    <h1>
+        <a href="{{ route('register') }}">Register</a> 
+    </h1>
+
+</body>
+</html>
